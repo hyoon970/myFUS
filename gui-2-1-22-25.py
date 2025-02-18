ea2c3f0d0d1c8d684b227ff1bbf5dc591b8faef2
Ethan Nguyen <ethannguyen@dhcp-10-29-221-98.dyn.MIT.EDU>
2025-02-17 21:38:39 -0500

    testing

--- gui-2-1-22-25.py
+++ gui-2-1-22-25.py
@@ -9,6 +9,7 @@ from sklearn.cluster import KMeans
 from collections import Counter
 
 # Initialize the ArUco dictionary and parameters
+#testing
 aruco_dict = aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
 parameters = aruco.DetectorParameters()
 detector = aruco.ArucoDetector(aruco_dict, parameters)
